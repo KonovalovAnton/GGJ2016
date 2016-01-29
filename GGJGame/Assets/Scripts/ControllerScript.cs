3301dc546b9972fcfbcfcfcb3bdf8de5b8dd1d8e
rostislavsmirnov <rostislavsmrnoff@gmail.com>
2016-01-30 01:51:48 +0300

    Combination with Nikitas code

---
+++ GGJGame/Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerScript : MonoBehaviour {
+
+	bool knightIsReady = false;
+	public DialogueScript UI;
+	int[] QTE;
+
+	void SetKnightReady() {
+		knightIsReady = true;
+		genQTE (3);
+		QTEController.instance.BeginValidation(QTE);
+	}
+
+	void genQTE(int length) {
+		QTE = new int[length];
+		for (int i = 0; i < length; i++) {
+			QTE [i] = Random.Range (0, 3);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (knightIsReady) {
+
+		}
+	}
+
+}
---
+++ NikitaMerge/Scripts/InputController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum InputMode {
+	Keyboard,
+	Joystick
+}
+
+public class InputController : Singleton<InputController> {
+	const string _newObjectName = "_InputController";
+
+	public InputMode mode;
+	public KeyCode[] joystickCodes;
+	public KeyCode[] keyboardCodes;
+
+	public Drums drums;
+
+	protected override string newObjectName {
+		get { return _newObjectName; }
+	}
+	
+	protected override bool dontDestroyOnLoad {
+		get { return false; }
+	}
+
+	protected override void OnAwake () {
+	}
+
+	void Update () {
+		for (int i = 0; i < joystickCodes.Length; i++) {
+			/*KeyCode code = KeyCode.A;
+			switch (mode) {
+			case InputMode.Joystick:
+				code = joystickCodes[i];
+			break;
+			case InputMode.Keyboard:
+				code = keyboardCodes[i];
+			break;
+			}*/
+
+			if (Input.GetKeyDown(joystickCodes[i]) || Input.GetKeyDown(keyboardCodes[i])) {
+					drums.PlayDrum(i);
+					QTEController.instance.TakeInput(i);
+					Debug.Log(i);
+			}
+		}
+	}
+}
