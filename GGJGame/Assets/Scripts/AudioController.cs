ffe5b304ef898c4d001d577ba9551ab94dc3434a
Антон Коновалов < >
2016-01-31 16:35:28 +0300

    commit before presentation

---
+++ GGJGame/Assets/Scripts/AudioController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	public AudioClip normalMusic;
+	public AudioClip heavyMusic;
+	public AudioSource audioSource;
+	public float delay;
+	bool normal = true;
+	float heavyMusicStart;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update() {
+		if (!normal && Time.time > heavyMusicStart + heavyMusic.length + delay) {
+			normal = true;
+			audioSource.Stop();
+			audioSource.loop = true;
+			audioSource.PlayOneShot(normalMusic);
+		}
+	}
+
+	public void OnHeadOff() {
+		if (!normal) {
+			return;
+		}
+		normal = false;
+		heavyMusicStart = Time.time;
+		audioSource.Stop();
+		audioSource.loop = false;
+		audioSource.PlayOneShot(heavyMusic);
+	}
+}
